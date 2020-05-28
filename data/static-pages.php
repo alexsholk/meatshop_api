58829db118b961ff0a45d0e25a60c9832dd8a8e7
Alexander Sholk <alexsholk@yandex.by>
2020-05-28 17:11:52 +0300

    Static pages api fixes

---
+++ data/static-pages.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'slug' => 'about',
+        'title' => 'О нас',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid illum 
+officiis repellat sed sit. Amet animi dolores ea eaque nulla officia pariatur perspiciatis quae recusandae sequi! 
+Harum laborum quia reprehenderit!</p>'
+    ],
+    [
+        'id' => 2,
+        'slug' => 'delivery',
+        'title' => 'Доставка',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt 
+dicta ea excepturi exercitationem ipsum laudantium qui quos totam veniam. Ab adipisci atque dicta dolorem dolorum et 
+exercitationem laborum minus, natus nesciunt nihil nostrum odio quae, quis similique ullam voluptatem. Voluptatem!</p>'
+    ],
+    [
+        'id' => 3,
+        'slug' => 'partnership',
+        'title' => 'Юр. лицам и ИП',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam 
+amet beatae doloribus, eveniet iste iure necessitatibus odio provident. Inventore, vero!</p>'
+    ],
+    [
+        'id' => 4,
+        'slug' => 'contacts',
+        'title' => 'Контакты',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium 
+architecto aspernatur at autem delectus et exercitationem facere hic ipsam, iste optio quaerat quod recusandae sit 
+vero voluptas voluptatibus voluptatum?</p>'
+    ],
+    [
+        'id' => 5,
+        'slug' => 'payment-details',
+        'title' => 'Реквизиты',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur at, corporis, 
+doloremque eligendi excepturi fugit necessitatibus pariatur porro quam, quasi quia quibusdam veniam? Ad aspernatur 
+delectus dicta ea eligendi fugit molestiae nemo nihil non, provident quaerat quos recusandae repudiandae suscipit?</p>'
+    ],
+    [
+        'id' => 6,
+        'slug' => 'ua',
+        'title' => 'Пользовательское соглашение',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore esse hic molestias 
+recusandae voluptas voluptates!</p>'
+    ],
+    [
+        'id' => 7,
+        'slug' => 'pdpa',
+        'title' => 'Соглашение на обработку персональных данных',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, aspernatur blanditiis 
+commodi consequuntur dolorum est ex expedita in mollitia, nemo nisi, optio totam unde. A adipisci aspernatur delectus, 
+deleniti odit quaerat quas similique vero voluptatibus.</p>'
+    ],
+    [
+        'id' => 8,
+        'slug' => '404',
+        'title' => 'Страница не найдена',
+        'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam 
+amet beatae doloribus, eveniet iste iure necessitatibus odio provident. Inventore, vero!</p>'
+    ],
+];
--- src/Controller/Api/StaticPageController.php
+++ src/Controller/Api/StaticPageController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Api;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,68 +12,33 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class StaticPageController
 {
-    private const STATIC_PAGES = [
-        [
-            'id' => 'about',
-            'title' => 'О нас',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid illum 
-officiis repellat sed sit. Amet animi dolores ea eaque nulla officia pariatur perspiciatis quae recusandae sequi! 
-Harum laborum quia reprehenderit!</p>'
-        ],
-        [
-            'id' => 'delivery',
-            'title' => 'Доставка',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt 
-dicta ea excepturi exercitationem ipsum laudantium qui quos totam veniam. Ab adipisci atque dicta dolorem dolorum et 
-exercitationem laborum minus, natus nesciunt nihil nostrum odio quae, quis similique ullam voluptatem. Voluptatem!</p>'
-        ],
-        [
-            'id' => 'partnership',
-            'title' => 'Юр. лицам и ИП',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam 
-amet beatae doloribus, eveniet iste iure necessitatibus odio provident. Inventore, vero!</p>'
-        ],
-        [
-            'id' => 'contacts',
-            'title' => 'Контакты',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium 
-architecto aspernatur at autem delectus et exercitationem facere hic ipsam, iste optio quaerat quod recusandae sit 
-vero voluptas voluptatibus voluptatum?</p>'
-        ],
-        [
-            'id' => 'payment-details',
-            'title' => 'Реквизиты',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur at, corporis, 
-doloremque eligendi excepturi fugit necessitatibus pariatur porro quam, quasi quia quibusdam veniam? Ad aspernatur 
-delectus dicta ea eligendi fugit molestiae nemo nihil non, provident quaerat quos recusandae repudiandae suscipit?</p>'
-        ],
-        [
-            'id' => 'ua',
-            'title' => 'Пользовательское соглашение',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore esse hic molestias 
-recusandae voluptas voluptates!</p>'
-        ],
-        [
-            'id' => 'pdpa',
-            'title' => 'Соглашение на обработку персональных данных',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, aspernatur blanditiis 
-commodi consequuntur dolorum est ex expedita in mollitia, nemo nisi, optio totam unde. A adipisci aspernatur delectus, 
-deleniti odit quaerat quas similique vero voluptatibus.</p>'
-        ],
-        [
-            'id' => '404',
-            'title' => 'Страница не найдена',
-            'content' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam 
-amet beatae doloribus, eveniet iste iure necessitatibus odio provident. Inventore, vero!</p>'
-        ],
-    ];
+    private $data;
+
+    public function __construct()
+    {
+        $this->data = require __DIR__ . '/../../../data/static-pages.php';
+    }
 
     /**
      * @Route("", methods={"GET"})
      */
-    public function list()
+    public function list(Request $request)
     {
-        return new JsonResponse(self::STATIC_PAGES);
+        $data = [];
+
+        if ($slug = $request->query->get('slug')) {
+            foreach ($this->data as $staticPage) {
+                if ($staticPage['slug'] == $slug) {
+                    $data[] = $staticPage;
+                }
+            }
+        } else {
+            $data = $this->data;
+        }
+
+        $response = new JsonResponse($data, Response::HTTP_OK);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 
     /**
@@ -80,13 +47,15 @@ amet beatae doloribus, eveniet iste iure necessitatibus odio provident. Inventor
     public function get($id)
     {
         $data = null;
-        foreach (self::STATIC_PAGES as $staticPage) {
-            if ($staticPage['id'] === $id) {
+
+        foreach ($this->data as $staticPage) {
+            if ($staticPage['id'] == $id) {
                 $data = $staticPage;
+                break;
             }
         }
 
-        $response = new JsonResponse($data);
+        $response = new JsonResponse($data, $data ? Response::HTTP_OK : Response::HTTP_NOT_FOUND);
         $response->headers->set('Access-Control-Allow-Origin', '*');
         return $response;
     }
