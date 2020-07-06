1274b59731193b5b1765cdeaeb86f13cff12b28e
ftp <otakoon_alexsholk@atom2.beget.ru>
2020-07-06 13:11:50 +0300

    email

--- src/Controller/Api/OrderController.php
+++ src/Controller/Api/OrderController.php
@@ -15,37 +15,47 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class OrderController
 {
+    const ORDER_LOG_PATH = __DIR__ . '/../../../data/orders/';
+    const ORDER_RECIPIENT_EMAIL = 'alexsholk@yandex.by';
+
     /**
      * @Route("", methods={"POST"})
      */
     public function create(Request $request, MailerInterface $mailer)
     {
-        /*
-        $data = json_decode($request->getContent(false), true);
+        $rawContent = $request->getContent(false);
+
+        // Log order
+        $this->logOrderRawContent($rawContent);
+        // Send email
+        $this->sendOrderEmail(json_decode($rawContent, true), $mailer);
+
+        return new JsonResponse(null, Response::HTTP_OK);
+    }
+
+    private function logOrderRawContent(string $rawContent)
+    {
+        $filename = date('Y_m_d_H_i') . '.json';
+        file_put_contents(
+            self::ORDER_LOG_PATH . $filename,
+            $rawContent
+        );
+    }
+
+    private function sendOrderEmail(array $data, MailerInterface $mailer)
+    {
         $mail = (new TemplatedEmail())
-            ->from('order@miloveat.ru')
-            ->to('alexsholk@yandex.by')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
+            ->from('shop@miloveat.ru')
+            ->to(self::ORDER_RECIPIENT_EMAIL)
             ->priority(Email::PRIORITY_HIGH)
             ->subject('Заказ')
             ->htmlTemplate('emails/order.html.twig')
             ->context([
-                'cart' => $data['cart'],
-                'form' => $data['form'],
+                'cart' => json_encode($data['cart'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
+                'form' => json_encode($data['form'], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
             ]);
 
         $mailer->send($mail);
-        */
-
-        $filename = date('Y_m_d_H_i') . '.json';
-
-        file_put_contents(
-            __DIR__ . '/../../../data/orders/' . $filename,
-            $request->getContent(false)
-        );
-        return new JsonResponse(null, Response::HTTP_OK);
     }
 
     /**
@@ -53,20 +63,9 @@ class OrderController
      */
     public function test(MailerInterface $mailer)
     {
-        $mail = (new TemplatedEmail())
-            ->from('shop@miloveat.ru')
-            ->to('alexsholk@yandex.by')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            ->priority(Email::PRIORITY_HIGH)
-            ->subject('Заказ')
-            ->htmlTemplate('emails/order.html.twig')
-            ->context([
-            ]);
+        $data = json_decode(file_get_contents(self::ORDER_LOG_PATH . '2020_07_05_03_12.json'), true);
+        $this->sendOrderEmail($data, $mailer);
 
-        $mailer->send($mail);
-
-        die('ok');
+        return new JsonResponse('OK', Response::HTTP_OK);
     }
 }
